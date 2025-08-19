20bd1233add2a3cba0680e709a05d1b828f48e9e
VishwaVikas2006 <vishwavikas2006@gmail.com>
2025-08-20 00:55:43 +0530

    Add .gitignore and clean node_modules from repo

--- src/App.jsx
+++ src/App.jsx
@@ -4,4 +4,8 @@ import './styles/App.css';
 
 const App = () => <Home />;
 
+<<<<<<< HEAD
 export default App;
+=======
+export default App;
+>>>>>>> e74565f (Add .gitignore and clean node_modules from repo)
